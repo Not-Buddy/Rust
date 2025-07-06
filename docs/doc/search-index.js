fe8080f787438a9a478ecc68f69c0dc5a5de2d88
Aary Kinge <139928169+Not-Buddy@users.noreply.github.com>
2025-07-05 23:38:28 -0500

    Delete docs directory

--- docs/doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = new Map(JSON.parse('[["lib",{"t":"","n":[],"q":[],"i":"","f":"","D":"`","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAAA","P":[]}],["lib",{"t":"H","n":["add"],"q":[[0,"lib"]],"i":"`","f":"{{bb}b}","D":"`","p":[[1,"u64"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
-if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
-else if (window.initSearch) window.initSearch(searchIndex);
-//{"start":39,"fragment_lengths":[130,167]}
