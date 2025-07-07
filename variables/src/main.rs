423faf29b276532853e663e5d014a3982136d049
NBWindows <kingeaary@gmail.com>
2025-07-07 17:15:57 -0500

    Mapping function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,7 @@
 fn main() 
 {
-    let x = 5;
+    let x: i32 = 5;
     println!("The value of x is: {}",x);
-    let x="six";
+    let x: &'static str="six";
     println!("The value of x now is: {}",x);
-
-    const COUNT:u32=100_000;
-
-
 }
