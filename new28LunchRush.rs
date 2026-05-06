bdd4931ddc6fc876adb6a8e0ab57721d0411d183
Not-Buddy <kingeaary@gmail.com>
2026-05-06 09:07:53 +0000

    fix for min val and higher datatype to handle bigger numbers now

--- new28LunchRush.rs
+++ new28LunchRush.rs
@@ -6,20 +6,19 @@ fn main(){
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let mut temp = input.split_whitespace();
-    let n : i32 = temp.next().unwrap().parse().unwrap();
-    let k : i32 = temp.next().unwrap().parse().unwrap();
+    let n : i64 = temp.next().unwrap().parse().unwrap();
+    let k : i64 = temp.next().unwrap().parse().unwrap();
     input.clear();
 
     //let mut rabbits : Vec<(i32, i32)> = Vec::new(); 
-    let mut max_val : i32 = -1;
+    let mut max_val : i64 = i64::MIN;
     for _ in 0..n{
         input.clear();
         io::stdin().read_line(&mut input).expect("Failed to read line");
-        let mut rabbit = input.split_whitespace().flat_map(|s| s.parse::<i32>());
+        let mut rabbit = input.split_whitespace().flat_map(|s| s.parse::<i64>());
 
 
-        let fi = rabbit.next().unwrap();
-        let ti = rabbit.next().unwrap();
+        let (fi, ti) = (rabbit.next().unwrap(),rabbit.next().unwrap());
         
         if ti > k{
             max_val = max(max_val, fi - (ti-k));
