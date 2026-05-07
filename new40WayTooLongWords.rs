73e365fd307309ab352c9ee512a5a0bfef209253
Not-Buddy <kingeaary@gmail.com>
2026-05-07 11:57:08 +0000

    strings are gae in rust especially read_line cause it includes the /n

--- new40WayTooLongWords.rs
+++ new40WayTooLongWords.rs
@@ -1,35 +1,26 @@
 use std::io;
 
-fn main(){
+fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let mut temp = input.split_whitespace();
-    let mut n : i32 = temp.next().unwrap().parse().unwrap();
-    input.clear();
-    
-    let mut strungs : Vec<String> = Vec::new();
-    for _ in 0..n{
-        input.clear();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let strung : String = input.clone();
-        strungs.push(strung);
-    }
+    io::stdin().read_line(&mut input).unwrap();
+    let n: i32 = input.trim().parse().unwrap();
 
-    for i in strungs{
-        if i.len()<10{
-            println!("{}",i);
-        }
-        else {
-            let mut chars = i.trim().chars();
+    for _ in 0..n {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        
+        let s = input.trim();
+        let true_len = s.len();
+
+  
+        if true_len > 10 {
+            let mut chars = s.chars();
             let first = chars.next().unwrap();
             let last = chars.last().unwrap();
-
-            let s = i.trim();
-            n = s.len() as i32;
-
-            println!("{}{}{}", first, n - 2, last);
+            
+            println!("{}{}{}", first, true_len - 2, last);
+        } else {
+            println!("{}", s);
         }
-
     }
-
 }
