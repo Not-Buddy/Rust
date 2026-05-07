920ada320a63dcccd02963946861ee0ffa327107
Not-Buddy <kingeaary@gmail.com>
2026-05-07 12:23:52 +0000

    who tf made y a vowel bruh

--- new41StringTask.rs
+++ new41StringTask.rs
@@ -3,16 +3,16 @@ use std::io;
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let temp : String = input.trim().to_lowercase().parse().unwrap();
+    let temp : String = input.trim().to_lowercase();
 
     let vec : Vec<char> = temp.chars().collect();
 
     let mut strung : String = String::new();
 
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
     for i in 0..vec.len(){
-        if vec[i] == 'a' || vec[i] == 'e' ||
-        vec[i] == 'i' || vec[i] == 'o' ||
-        vec[i] == 'u'{
+        if vowels.contains(&vec[i]){
             continue;
         }
         else {
