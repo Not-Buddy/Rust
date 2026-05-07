db86151ca90785aec45cefeb6518ab8909de8e9e
Not-Buddy <kingeaary@gmail.com>
2026-05-07 13:06:15 +0000

    apparently sorting wasnt needed

--- new45PetyaAndStrings.rs
+++ new45PetyaAndStrings.rs
@@ -3,15 +3,12 @@ use std::io;
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let mut vec1 : Vec<char> = input.trim().to_lowercase().chars().collect();
+    let vec1 : Vec<char> = input.trim().to_lowercase().chars().collect();
     input.clear();
 
     io::stdin().read_line(&mut input).unwrap();
-    let mut vec2 : Vec<char> = input.trim().to_lowercase().chars().collect();
+    let vec2 : Vec<char> = input.trim().to_lowercase().chars().collect();
     input.clear();
-
-    vec1.sort();
-    vec2.sort();
 
     if vec1 == vec2{
         println!("0");
